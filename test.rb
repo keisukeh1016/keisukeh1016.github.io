b48763bfa77f20b31170d2f242caaf2e5c0261a5
keisuke_ubuntu <57240541+keisukeh1016@users.noreply.github.com>
2020-05-31 00:11:50 +0900

    hello

--- test.rb
+++ test.rb
@@ -1,1 +1,6 @@
-good bye taro
+require 'open-uri'
+require 'nokogiri'
+require 'roo'
+
+html = URI.open("https://www.bloomberg.co.jp/quote/4689:JP")
+p Nokogiri::HTML(html).css(".data-table_detailed>div:nth-child(4)>div:nth-child(2)").to_s.match(/>\s(.*)\s</)[1].delete(",").to_f
