7765932d52bb12394cc22aaaf253bb7bdf83b9c0
Hattori Keisuke <57240541+keisukeh1016@users.noreply.github.com>
2022-02-19 13:10:47 +0900

    Windowsアプリ一覧を更新

--- windows/h-Install-Windows-Apps.ps1
+++ windows/h-Install-Windows-Apps.ps1
@@ -1,16 +1,23 @@
 winget install --exact --id "7zip.7zip" --silent
+winget install --exact --id "DBBrowserForSQLite.DBBrowserForSQLite" --silent
 winget install --exact --id "Git.Git" --silent
 winget install --exact --id "GitHub.cli" --silent
 winget install --exact --id "Google.Chrome" --silent
+winget install --exact --id "Google.ChromeRemoteDesktop" --silent
 winget install --exact --id "Microsoft.PowerToys" --silent
 winget install --exact --id "Microsoft.VC++2015-2022Redist-x64" --silent
 winget install --exact --id "Microsoft.VC++2015-2022Redist-x86" --silent
 winget install --exact --id "Microsoft.VisualStudio.2022.Community" --silent
 winget install --exact --id "Microsoft.VisualStudioCode" --silent
 winget install --exact --id "Microsoft.WindowsTerminal" --silent
+winget install --exact --id "Mozilla.Firefox" --silent
+winget install --exact --id "Mozilla.Thunderbird" --silent
 winget install --exact --id "Oracle.VirtualBox" --silent
+winget install --exact --id "Postman.Postman" --silent
 winget install --exact --id "Spotify.Spotify" --silent
 winget install --exact --id "TeamViewer.TeamViewer" --silent
 winget install --exact --id "Volta.Volta" --silent
 winget install --exact --id "WinMerge.WinMerge" --silent
 winget install --exact --id "Zoom.Zoom" --silent
+
+# MarketSpeed2 https://marketspeed.jp/ms2/
