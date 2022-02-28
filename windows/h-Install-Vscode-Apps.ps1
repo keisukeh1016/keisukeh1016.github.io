eea722eb71b1e9711661d4816d352c5371beb88e
Hattori Keisuke <57240541+keisukeh1016@users.noreply.github.com>
2022-02-28 21:18:44 +0900

    VSCode拡張機能リストを追加

--- windows/h-Install-Vscode-Apps.ps1
+++ windows/h-Install-Vscode-Apps.ps1
@@ -1,6 +1,10 @@
-code --install-extension Angular.ng-template
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension GrapeCity.gc-excelviewer
-code --install-extension MS-CEINTL.vscode-language-pack-ja
-code --install-extension ms-vscode.PowerShell
+code --install-extension 'Angular.ng-template'
+code --install-extension 'eamodio.gitlens'
+code --install-extension 'esbenp.prettier-vscode'
+code --install-extension 'GrapeCity.gc-excelviewer'
+code --install-extension 'MS-CEINTL.vscode-language-pack-ja'
+code --install-extension 'ms-dotnettools.csharp'
+code --install-extension 'ms-vscode.hexeditor'
+code --install-extension 'ms-vscode.PowerShell'
+code --install-extension 'octref.vetur'
+code --install-extension 'VisualStudioExptTeam.vscodeintellicode'
