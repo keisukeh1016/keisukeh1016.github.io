10afa9a4d48e29595c72f1bbbed4aa914ea73098
Hattori Keisuke <57240541+keisukeh1016@users.noreply.github.com>
2022-03-23 22:24:49 +0900

    アプリ一覧を更新

--- windows/h-Install-Windows-Apps.ps1
+++ windows/h-Install-Windows-Apps.ps1
@@ -1,10 +1,12 @@
 winget install --exact --id "7zip.7zip" --silent
 winget install --exact --id "DBBrowserForSQLite.DBBrowserForSQLite" --silent
+winget install --exact --id "EpicGames.EpicGamesLauncher" --silent
 winget install --exact --id "Git.Git" --silent
 winget install --exact --id "GitHub.cli" --silent
 winget install --exact --id "Google.Chrome" --silent
 winget install --exact --id "Google.ChromeRemoteDesktop" --silent
 winget install --exact --id "Microsoft.PowerToys" --silent
+winget install --exact --id "Microsoft.SQLServer.2019.Developer" --silent
 winget install --exact --id "Microsoft.SQLServerManagementStudio" --silent
 winget install --exact --id "Microsoft.VC++2015-2022Redist-x64" --silent
 winget install --exact --id "Microsoft.VC++2015-2022Redist-x86" --silent
@@ -17,6 +19,7 @@ winget install --exact --id "Oracle.VirtualBox" --silent
 winget install --exact --id "Postman.Postman" --silent
 winget install --exact --id "Spotify.Spotify" --silent
 winget install --exact --id "TeamViewer.TeamViewer" --silent
+winget install --exact --id "Volta.Volta" --silent
 winget install --exact --id "WinMerge.WinMerge" --silent
 winget install --exact --id "Zoom.Zoom" --silent
 
