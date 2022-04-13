2e8e356dfd7bca2fe9673e04c86549b906d06c51
keisuke <57240541+keisukeh1016@users.noreply.github.com>
2022-04-13 20:48:19 +0900

    Windowsアプリ一覧を更新

--- windows/h-Install-Windows-Apps.ps1
+++ windows/h-Install-Windows-Apps.ps1
@@ -10,7 +10,8 @@ winget install --exact --id "Google.ChromeRemoteDesktop" --silent
 winget install --exact --id "LINE.LINE" --silent
 winget install --exact --id "Microsoft.PowerToys" --silent
 winget install --exact --id "Microsoft.SQLServer.2019.Developer" --silent
-winget install --exact --id "Microsoft.SQLServerManagementStudio" --silent
+winget install --exact --id "Microsoft.SQLServerManagementStudio" --locale "ja-JP" --silent
+# winget install --exact --id "Microsoft.SQLServerManagementStudio" --locale "en-US" --silent --version "14.0.17289.0"
 winget install --exact --id "Microsoft.VC++2015-2022Redist-x64" --silent
 winget install --exact --id "Microsoft.VC++2015-2022Redist-x86" --silent
 winget install --exact --id "Microsoft.VisualStudio.2022.Community" --silent
@@ -27,8 +28,17 @@ winget install --exact --id "Volta.Volta" --silent
 winget install --exact --id "WinMerge.WinMerge" --silent
 winget install --exact --id "Zoom.Zoom" --silent
 
+# サクラエディタ
+# https://sakura-editor.github.io/
+
 # MarketSpeed2
 # https://marketspeed.jp/ms2/
 
 # NVM for Windows
 # https://github.com/coreybutler/nvm-windows/releases
+
+# SQL Server Management Studio 14.0.17289.0
+# https://docs.microsoft.com/ja-jp/sql/ssms/release-notes-ssms?view=sql-server-ver15
+
+# TeamViewer 12
+# https://www.teamviewer.com/ja/download/previous-versions/
