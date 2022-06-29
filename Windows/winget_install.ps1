b464557ca505da11284ba3b7414ae959baba98b3
keisuke <57240541+keisukeh1016@users.noreply.github.com>
2022-06-29 20:27:38 +0900

    update winget

--- Windows/winget_install.ps1
+++ Windows/winget_install.ps1
@@ -1,34 +1,36 @@
 winget install --exact --id "7zip.7zip" --silent
-winget install --exact --id "Adobe.Acrobat.Reader.32-bit" --silent
-winget install --exact --id "BraveSoftware.BraveBrowser" --silent
+# winget install --exact --id "Adobe.Acrobat.Reader.32-bit" --silent
+# winget install --exact --id "BraveSoftware.BraveBrowser" --silent
 # winget install --exact --id "CoreyButler.NVMforWindows" --silent
 winget install --exact --id "DBBrowserForSQLite.DBBrowserForSQLite" --silent
-winget install --exact --id "Discord.Discord" --silent
+# winget install --exact --id "Discord.Discord" --silent
 winget install --exact --id "EpicGames.EpicGamesLauncher" --silent
 winget install --exact --id "GIMP.GIMP" --silent
 winget install --exact --id "Git.Git" --silent
 winget install --exact --id "GitHub.cli" --silent
 winget install --exact --id "Google.Chrome" --silent
-winget install --exact --id "Google.ChromeRemoteDesktop" --silent
-winget install --exact --id "LINE.LINE" --silent
+# winget install --exact --id "Google.ChromeRemoteDesktop" --silent
+# winget install --exact --id "LINE.LINE" --silent
+# winget install --exact --id "Microsoft.PowerShell" --silent
 winget install --exact --id "Microsoft.PowerToys" --silent
-winget install --exact --id "Microsoft.SQLServer.2019.Developer" --silent
+# winget install --exact --id "Microsoft.SQLServer.2019.Developer" --silent
 winget install --exact --id "Microsoft.SQLServerManagementStudio" --locale "ja-JP" --silent
-winget install --exact --id "Microsoft.VC++2015-2022Redist-x64" --silent
-winget install --exact --id "Microsoft.VC++2015-2022Redist-x86" --silent
+# winget install --exact --id "Microsoft.VC++2015-2022Redist-x64" --silent
+# winget install --exact --id "Microsoft.VC++2015-2022Redist-x86" --silent
 winget install --exact --id "Microsoft.VisualStudio.2022.Community" --silent
 winget install --exact --id "Microsoft.VisualStudioCode" --silent
-winget install --exact --id "Microsoft.WindowsSDK" --silent
-winget install --exact --id "Microsoft.WindowsTerminal" --silent
-winget install --exact --id "Mozilla.Firefox" --silent
-winget install --exact --id "Mozilla.Thunderbird" --silent
-winget install --exact --id "Oracle.VirtualBox" --silent
+# winget install --exact --id "Microsoft.WindowsSDK" --silent
+# winget install --exact --id "Microsoft.WindowsTerminal" --silent
+# winget install --exact --id "Mozilla.Firefox" --silent
+# winget install --exact --id "Mozilla.Thunderbird" --silent
+# winget install --exact --id "Oracle.VirtualBox" --silent
 winget install --exact --id "Postman.Postman" --silent
-winget install --exact --id "Spotify.Spotify" --silent
+# winget install --exact --id "Sayuri.FFFTP" --silent
+# winget install --exact --id "Spotify.Spotify" --silent
 winget install --exact --id "TeamViewer.TeamViewer" --silent
 winget install --exact --id "Volta.Volta" --silent
 winget install --exact --id "WinMerge.WinMerge" --silent
-winget install --exact --id "Zoom.Zoom" --silent
+# winget install --exact --id "Zoom.Zoom" --silent
 
 # DevToys
 winget install --exact --id "9PGCV4V3BK4W" --silent
