7d8762900bceb21356a8cf1455446933044accdb
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-08-17 13:12:07 +0900

    Update choco-install.ps1

--- Windows/choco-install.ps1
+++ Windows/choco-install.ps1
@@ -1,12 +1,21 @@
 choco install 7zip --yes
 choco install adobereader --yes
 choco install brave --yes
+choco install chrome-remote-desktop-host --yes
 choco install discord --yes
 choco install epicgameslauncher --yes
 choco install gh --yes
 choco install gimp --yes
 choco install git --yes
 choco install github-desktop --yes
+choco install googlechrome --yes
+choco install line --yes
 choco install nvm --yes
+choco install powertoys --yes
+choco install pwsh --yes
 choco install sqlitebrowser --yes
 choco install tablacus --yes
+
+
+# choco install sql-server-express --yes
+# choco install sql-server-management-studio --yes
