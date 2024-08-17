da885f4c41f61b8a54793f915082aee968740632
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-08-17 15:14:43 +0900

    Update choco-install.ps1

--- Windows/choco-install.ps1
+++ Windows/choco-install.ps1
@@ -4,20 +4,29 @@ choco install brave --yes
 choco install chrome-remote-desktop-host --yes
 choco install discord --yes
 choco install epicgameslauncher --yes
+choco install ffftp --yes
+choco install firefox --yes --params "/NoTaskbarShortcut /NoDesktopShortcut"
 choco install gh --yes
 choco install gimp --yes
 choco install git --yes
 choco install github-desktop --yes
 choco install googlechrome --yes
 choco install line --yes
+choco install microsoft-windows-terminal --yes
 choco install nvm --yes
+choco install postman --yes
 choco install powertoys --yes
 choco install pwsh --yes
 choco install sqlitebrowser --yes
 choco install tablacus --yes
+choco install teamviewer --yes
+choco install thunderbird --yes --params "/NoTaskbarShortcut /NoDesktopShortcut"
 choco install vcredist140 --yes
+choco install virtualbox --yes --params "/NoDesktopShortcut"
+choco install volta --yes 
 choco install vscode --yes --params "/NoDesktopIcon"
-choco install microsoft-windows-terminal --yes
+choco install winmerge -yes
+choco install zoom --yes --params "/NoDesktopShortcut"
 
 
 # choco install sql-server-express --yes
