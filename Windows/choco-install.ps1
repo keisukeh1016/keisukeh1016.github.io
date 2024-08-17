7b459b870b2d7718f45cdbb04810f4cb4badc9e4
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-08-17 16:50:15 +0900

    Update choco-install.ps1

--- Windows/choco-install.ps1
+++ Windows/choco-install.ps1
@@ -28,27 +28,3 @@ choco install volta --yes
 choco install vscode --yes --params "/NoDesktopIcon"
 choco install winmerge -yes
 choco install zoom --yes --params "'/NoDesktopShortcut'"
-
-# SQL Server 2022 Express
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://download.microsoft.com/download/5/1/4/5145fe04-4d30-4b85-b0d1-39533663a2f1/SQL2022-SSEI-Expr.exe" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads")
-
-# SQL Server Management Studio
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://download.microsoft.com/download/9/b/e/9bee9f00-2ee2-429a-9462-c9bc1ce14c28/SSMS-Setup-JPN.exe" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads")
-
-# Visual Studio 2022 Community
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Release&version=VS2022&source=VSLandingPage&cid=2030:0f9259c33556402991213d3b2a461252" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads")
-
-# マーケットスピード II
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://download.rakuten-sec.co.jp/MarketSpeed2Installkits_0001.exe" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads")
