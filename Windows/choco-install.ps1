0c82d7d2e31256b66cd56fa21980364bda682be5
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-08-17 11:35:15 +0900

    Update choco-install.ps1

--- Windows/choco-install.ps1
+++ Windows/choco-install.ps1
@@ -1,6 +1,7 @@
 choco install 7zip --yes
 choco install adobereader --yes
 choco install brave --yes
+choco install discord --yes
 choco install nvm --yes
 choco install sqlitebrowser --yes
 choco install tablacus --yes
