6e49c7b7bba819d2240338cd325ffcf96700c682
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-08-19 01:11:25 +0900

    Update manual-install.ps1

--- Windows/manual-install.ps1
+++ Windows/manual-install.ps1
@@ -1,4 +1,5 @@
-New-Item ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup")
+$download = ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup") 
+New-Item $download -ItemType "directory"
 
 # Chocolatey 
 Set-ExecutionPolicy Bypass -Scope Process -Force
@@ -9,23 +10,23 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://com
 Invoke-WebRequest `
   -UseBasicParsing `
   -Uri "https://dl.honto.jp/pcapp/honto_setup.exe" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup")
+  -OutFile $download
 
 # HYPER SBI 2
 Invoke-WebRequest `
   -UseBasicParsing `
   -Uri "https://data.sbisec.co.jp/hsbi2/HYPER_SBI_2_Setup.exe" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup")
+  -OutFile $download
 
 # VMware Workstation Pro
 Invoke-WebRequest `
   -UseBasicParsing `
   -Uri "https://downloads2.broadcom.com/?file=VMware-workstation-full-17.5.2-23775571.exe&oid=30869714&id=HXLkQ3vjV75SG7X1f3xYY3YHIRihG9wIQ7R0YHLN1VtK5k8a0ZGdZVTpq2jZreZ2VxJu&verify=1723971972-LcgLeT%2BUhNHAsv2lGlWjuZgUJWoCCW72PoD3%2BRo3lZI%3D" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup")
+  -OutFile $download
 
 # マーケットスピード II
 Invoke-WebRequest `
   -UseBasicParsing `
   -Uri "https://download.rakuten-sec.co.jp/MarketSpeed2Installkits_0001.exe" `
-  -OutFile ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup")
+  -OutFile $download
   
