bbc63ed69082a0c2918554418112961cd68e1ef6
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-08-19 02:02:14 +0900

    Update manual-install.ps1

--- Windows/manual-install.ps1
+++ Windows/manual-install.ps1
@@ -1,5 +1,6 @@
 $download = ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) + "\Downloads\Setup") 
 New-Item $download -ItemType "directory"
+Set-Location $download
 
 # Chocolatey 
 Set-ExecutionPolicy Bypass -Scope Process -Force
@@ -7,26 +8,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # honto ビューアアプリ
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://dl.honto.jp/pcapp/honto_setup.exe" `
-  -OutFile $download
+curl.exe -LO "https://dl.honto.jp/pcapp/honto_setup.exe"
 
 # HYPER SBI 2
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://data.sbisec.co.jp/hsbi2/HYPER_SBI_2_Setup.exe" `
-  -OutFile $download
+curl.exe -LO "https://data.sbisec.co.jp/hsbi2/HYPER_SBI_2_Setup.exe"
 
 # VMware Workstation Pro
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://downloads2.broadcom.com/?file=VMware-workstation-full-17.5.2-23775571.exe&oid=30869714&id=HXLkQ3vjV75SG7X1f3xYY3YHIRihG9wIQ7R0YHLN1VtK5k8a0ZGdZVTpq2jZreZ2VxJu&verify=1723971972-LcgLeT%2BUhNHAsv2lGlWjuZgUJWoCCW72PoD3%2BRo3lZI%3D" `
-  -OutFile $download
+curl.exe -LO "https://downloads2.broadcom.com/?file=VMware-workstation-full-17.5.2-23775571.exe&oid=30869714&id=HXLkQ3vjV75SG7X1f3xYY3YHIRihG9wIQ7R0YHLN1VtK5k8a0ZGdZVTpq2jZreZ2VxJu&verify=1723971972-LcgLeT%2BUhNHAsv2lGlWjuZgUJWoCCW72PoD3%2BRo3lZI%3D"
 
 # マーケットスピード II
-Invoke-WebRequest `
-  -UseBasicParsing `
-  -Uri "https://download.rakuten-sec.co.jp/MarketSpeed2Installkits_0001.exe" `
-  -OutFile $download
+curl.exe -LO "https://download.rakuten-sec.co.jp/MarketSpeed2Installkits_0001.exe"
   
