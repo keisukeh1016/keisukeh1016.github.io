a51447b0c86f79cea720dae9cb9355450a5adafc
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-10-25 07:50:54 +0900

    update winget.ps1

--- Windows/install/choco.ps1
+++ Windows/install/choco.ps1
@@ -4,12 +4,7 @@ $User = [System.Environment]::GetEnvironmentVariable("Path", [System.Environment
 $Env:Path = "$Machine;$User"
 
 # Install
-choco install --yes 7zip
-choco install --yes bitwarden
 choco install --yes docker-desktop
-choco install --yes gh
-choco install --yes git
-choco install --yes googlechrome
 choco install --yes kindle
 choco install --yes nvm
 choco install --yes postman
@@ -17,19 +12,6 @@ choco install --yes sqlitebrowser
 choco install --yes tablacus
 choco install --yes winmerge
 
-# choco install --yes adobereader
-# choco install --yes brave
-# choco install --yes chrome-remote-desktop-host
-# choco install --yes discord
-# choco install --yes epicgameslauncher
-# choco install --yes ffftp
-# choco install --yes firefox
-# choco install --yes github-desktop
-# choco install --yes line
-# choco install --yes sakuraeditor
 # choco install --yes slack
-# choco install --yes teamviewer
-# choco install --yes thunderbird
-# choco install --yes virtualbox
 # choco install --yes volta 
-# choco install --yes zoom
+
