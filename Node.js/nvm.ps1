c18b5b2ece37f965b662b3277f2dcb719f7ec35c
keisukeh1016 <57240541+keisukeh1016@users.noreply.github.com>
2024-10-27 21:31:34 +0900

    update

--- Node.js/nvm.ps1
+++ Node.js/nvm.ps1
@@ -1,10 +1,10 @@
-function reloadPath {
-  $MachinePath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
-  $UserPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
-  $Env:Path = $MachinePath + ";" + $UserPath
+function reloadProcessPath {
+  $Machine = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+  $User = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+  $Env:Path = "$Machine;$User"
 }
 
-reloadPath
+reloadProcessPath
 
 nvm install 14.19.2
 nvm use 14.19.2
